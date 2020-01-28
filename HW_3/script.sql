e4f983f97424a5f303ba1cb19bf730dd5a962d6c
batvale1 <batvale1@yandex.ru>
2020-01-28 13:11:12 +0300

    Сделал третье ДЗ

---
+++ HW_3/script.sql
@@ -0,0 +1,235 @@
+DROP DATABASE IF EXISTS vk;
+CREATE DATABASE vk;
+USE vk;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY, -- SERIAL = BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE
+    firstname VARCHAR(50),
+    lastname VARCHAR(50) COMMENT 'Фамиль', -- COMMENT на случай, если имя неочевидное
+    email VARCHAR(120) UNIQUE,
+    phone BIGINT, 
+    INDEX users_phone_idx(phone), -- как выбирать индексы?
+    INDEX users_firstname_lastname_idx(firstname, lastname)
+);
+
+DROP TABLE IF EXISTS `profiles`;
+CREATE TABLE `profiles` (
+	user_id SERIAL PRIMARY KEY,
+    gender CHAR(1),
+    birthday DATE,
+	photo_id BIGINT UNSIGNED NULL,
+    created_at DATETIME DEFAULT NOW(),
+    hometown VARCHAR(100),
+    FOREIGN KEY (user_id) REFERENCES users(id) -- что за зверь в целом?
+    	ON UPDATE CASCADE -- как это работает? Какие варианты?
+    	ON DELETE restrict -- как это работает? Какие варианты?
+    -- , FOREIGN KEY (photo_id) REFERENCES media(id) -- пока рано, т.к. таблицы media еще нет
+);
+
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (
+	id SERIAL PRIMARY KEY,
+	from_user_id BIGINT UNSIGNED NOT NULL,
+    to_user_id BIGINT UNSIGNED NOT NULL,
+    body TEXT,
+    created_at DATETIME DEFAULT NOW(), -- можно будет даже не упоминать это поле при вставке
+    INDEX messages_from_user_id (from_user_id),
+    INDEX messages_to_user_id (to_user_id),
+    FOREIGN KEY (from_user_id) REFERENCES users(id),
+    FOREIGN KEY (to_user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS friend_requests;
+CREATE TABLE friend_requests (
+	-- id SERIAL PRIMARY KEY, -- изменили на композитный ключ (initiator_user_id, target_user_id)
+	initiator_user_id BIGINT UNSIGNED NOT NULL,
+    target_user_id BIGINT UNSIGNED NOT NULL,
+    -- `status` TINYINT UNSIGNED,
+    `status` ENUM('requested', 'approved', 'unfriended', 'declined'),
+    -- `status` TINYINT UNSIGNED, -- в этом случае в коде хранили бы цифирный enum (0, 1, 2, 3...)
+	requested_at DATETIME DEFAULT NOW(),
+	confirmed_at DATETIME,
+	
+    PRIMARY KEY (initiator_user_id, target_user_id),
+	INDEX (initiator_user_id), -- потому что обычно будем искать друзей конкретного пользователя
+    INDEX (target_user_id),
+    FOREIGN KEY (initiator_user_id) REFERENCES users(id),
+    FOREIGN KEY (target_user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS communities;
+CREATE TABLE communities(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(150),
+
+	INDEX communities_name_idx(name)
+);
+
+DROP TABLE IF EXISTS users_communities;
+CREATE TABLE users_communities(
+	user_id BIGINT UNSIGNED NOT NULL,
+	community_id BIGINT UNSIGNED NOT NULL,
+  
+	PRIMARY KEY (user_id, community_id), -- чтобы не было 2 записей о пользователе и сообществе
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (community_id) REFERENCES communities(id)
+);
+
+DROP TABLE IF EXISTS media_types;
+CREATE TABLE media_types(
+	id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+    -- записей мало, поэтому индекс будет лишним (замедлит работу)!
+);
+
+DROP TABLE IF EXISTS media;
+CREATE TABLE media(
+	id SERIAL PRIMARY KEY,
+    media_type_id BIGINT UNSIGNED NOT NULL,
+    user_id BIGINT UNSIGNED NOT NULL,
+  	body text,
+    filename VARCHAR(255),
+    size INT,
+	metadata JSON,
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    INDEX (user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (media_type_id) REFERENCES media_types(id)
+);
+
+DROP TABLE IF EXISTS likes;
+CREATE TABLE likes(
+	id SERIAL PRIMARY KEY,
+    user_id BIGINT UNSIGNED NOT NULL,
+    media_id BIGINT UNSIGNED NOT NULL,
+    created_at DATETIME DEFAULT NOW()
+
+    -- PRIMARY KEY (user_id, media_id) – можно было и так вместо id в качестве PK
+  	-- слишком увлекаться индексами тоже опасно, рациональнее их добавлять по мере необходимости (напр., провисают по времени какие-то запросы)  
+
+/* намеренно забыли, чтобы увидеть нехватку в ER-диаграмме
+    , FOREIGN KEY (user_id) REFERENCES users(id)
+    , FOREIGN KEY (media_id) REFERENCES media(id)
+*/
+);
+
+DROP TABLE IF EXISTS `photo_albums`;
+CREATE TABLE `photo_albums` (
+	`id` SERIAL,
+	`name` varchar(255) DEFAULT NULL,
+    `user_id` BIGINT UNSIGNED DEFAULT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+  	PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `photos`;
+CREATE TABLE `photos` (
+	id SERIAL PRIMARY KEY,
+	`album_id` BIGINT unsigned NOT NULL,
+	`media_id` BIGINT unsigned NOT NULL,
+
+	FOREIGN KEY (album_id) REFERENCES photo_albums(id),
+    FOREIGN KEY (media_id) REFERENCES media(id)
+);
+
+-------------- МОЕ ДЗ 3 (Вебинар Введение в проектирование БД) ---------------------------
+-- Таблица №1. Встречи.
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE `events` (
+	id SERIAL PRIMARY KEY,
+	description TEXT, -- описание события
+	event_data DATETIME, -- дата события
+	creator_id BIGINT UNSIGNED NOT NULL, -- создатель события
+	created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (creator_id) REFERENCES users(id),
+    INDEX (creator_id) -- индекс по создателю
+);
+
+-- Таблица №2. Участники встреч
+DROP TABLE IF EXISTS `event_participants`;
+CREATE TABLE `event_participants` (
+	event_id BIGINT UNSIGNED NOT NULL, -- событие
+	user_id BIGINT UNSIGNED NOT NULL, -- пользователь
+	status ENUM('Yes', 'Probably, yes', 'No'), -- статус участия пользователя. Да, нет, может быть.	
+	PRIMARY KEY (event_id, user_id), -- связка пользователь-событие должна быть уникальна
+	FOREIGN KEY (event_id) REFERENCES events(id), -- внешний ключ на событие
+	FOREIGN KEY (user_id) REFERENCES users(id), -- внеший ключ на участника
+	INDEX (event_id, user_id) -- индекс на событие и пользователя
+);
+
+-- Таблица №3. Игры.
+DROP TABLE IF EXISTS `games`;
+CREATE TABLE `games` (
+	id SERIAL PRIMARY KEY,
+	description TEXT, -- описание игры
+	publisher BIGINT UNSIGNED NOT NULL -- издатель
+);
+
+-- Таблица №4. Игроки
+DROP TABLE IF EXISTS `game_users`;
+CREATE TABLE `game_users` (
+	game_id BIGINT UNSIGNED NOT NULL, -- игра
+	user_id BIGINT UNSIGNED NOT NULL, -- пользователь
+	PRIMARY KEY (game_id, user_id), -- связка пользователь-игра должна быть уникальна
+	FOREIGN KEY (game_id) REFERENCES events(id), -- внешний ключ на игру
+	FOREIGN KEY (user_id) REFERENCES users(id), -- внеший ключ на участника
+	INDEX (game_id, user_id) -- индекс на игру и пользователя
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
