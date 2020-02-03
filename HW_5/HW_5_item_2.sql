456b27c702c8b82552ecdb3babe11307e3b456ad
batvale1 <batvale1@yandex.ru>
2020-02-03 16:10:32 +0300

    сделал пятое ДЗ

--- HW_5/HW_5_item_2.sql
+++ HW_5/HW_5_item_2.sql
@@ -12,7 +12,7 @@ create table users  (
 );
 -- наполним ее тестовыми данными
 INSERT into users (name, created_at, updated_at) values ('Josh', '20.10.2017 8:10', '20.07.2016 8:10');
-INSERT into users (name, created_at, updated_at) values ('Lisa', '04.07.2014 8:40', '20.10.2017 8:10');
+INSERT into users (name, created_at, updated_at) values ('Lisa', '04.07.2014 18:40', '20.10.2017 13:10');
 
 -- переименовываем изначальные поля
 ALTER table users rename column created_at to created_at_initial, RENAME COLUMN updated_at to updated_at_initial; 
@@ -20,8 +20,8 @@ ALTER table users rename column created_at to created_at_initial, RENAME COLUMN 
 ALTER table users add created_at datetime, ADD updated_at datetime;
 -- заполняем вновь созданные поля по шаблону
 UPDATE users set 
-created_at = STR_TO_DATE(created_at_initial, '%d.%m.%Y %h:%i'),
-updated_at = STR_TO_DATE(updated_at_initial, '%d.%m.%Y %h:%i');
+created_at = STR_TO_DATE(created_at_initial, '%d.%m.%Y %H:%i'),
+updated_at = STR_TO_DATE(updated_at_initial, '%d.%m.%Y %H:%i');
 -- удаляем изначальные колонки
 ALTER table users drop created_at_initial, DROP COLUMN updated_at_initial;
 
